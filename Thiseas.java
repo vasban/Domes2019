85e7519342c1e2227e00bfe56361ea017ba4c68a
Vasileios Mpanakos <vasilisbanakos1996@gmail.com>
2019-11-27 18:45:17 +0200

    new files

--- Thiseas.java
+++ Thiseas.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class Thiseas{
     public static void main (String [] args){
 
-       File f = new File("lavirinthos.txt");
+       File f = new File(args[0]);
        String line;
        int x ; int y; //diastaseis pinaka
        int entrancex; int entrancey;
@@ -43,24 +43,26 @@ public class Thiseas{
            int i=0;
            int j=0;
            while((line = buffreader.readLine()) != null){
+                if(i>=x ){
+                   System.out.println("Invalid labyrinth dimensions");
+                   System.out.println("The program is closing...");
+                   System.exit(0);
+               } 
+
+
                j=0;
                 // System.out.println(line);
                  st = new StringTokenizer(line);
                 while(st.hasMoreTokens()){
+                    if(j>=y ){
+                        System.out.println("Invalid labyrinth dimesions");
+                        System.out.println("The program is closing...");
+                        System.exit(0);
+                   }
                     labyrinth[i][j]= st.nextToken().charAt(0);
                    j++;  
-                //    if(j>y && st.hasMoreTokens()){
-                //         System.out.println("Invalid labyrinth dimesions");
-                //         System.out.println("The program is closing...");
-                //         System.exit(0);
-                //    }
                 }
                i++;
-            //    if(i>x && st.hasMoreTokens()){
-            //        System.out.println("Invalid labyrinth dimensions");
-            //        System.out.println("The program is closing...");
-            //        System.exit(0);
-            //    } 
            }
            System.out.println("-----------------------------------------");
            
@@ -73,19 +75,65 @@ public class Thiseas{
              System.out.println("\n"); 
            }
             // Checking the entrance
-           if(labyrinth[entrancex][entrancey]!='E'||labyrinth[entrancex][entrancey]!='e'){
+           if(labyrinth[entrancex][entrancey]!='e')labyrinth[entrancex][entrancey]='E';
+           if(labyrinth[entrancex][entrancey]!='E'){
                System.out.println("wrong entrance");
                System.out.println("The program is closing...");
                System.exit(0);
            }
 
-           
-           
+        StringStackImpl stack = new StringStackImpl();
+        stack.push(String.valueOf(labyrinth[entrancex][entrancey]));
+        boolean flag = false;
+        int movex = entrancex; int movey=entrancey;
+        // boolean b1 ,b2;
+        while(!flag){
+            if((movex+1)<x && labyrinth[movex+1][movey]=='0'){
+                movex+=1;
+                stack.push(String.valueOf(labyrinth[movex][movey]));//edw prepei na kanw tsek ama einai staurodromi
+                if(labyrinth[movex][movey]!='E')labyrinth[movex][movey] = 'V';
+                if(movex+1==x)flag = true;
+            }else if((movex-1)>=0 && labyrinth[movex-1][movey]=='0'){
+                movex-=1;
+                stack.push(String.valueOf(labyrinth[movex][movey]));
+                if(labyrinth[movex][movey]!='E')labyrinth[movex][movey] = 'V';
+                if(movex==0)flag = true;
+            }else if((movey+1)<y && labyrinth[movex][movey+1]=='0'){
+                movey+=1;
+                stack.push(String.valueOf(labyrinth[movex][movey]));
+                if(labyrinth[movex][movey]!='E')labyrinth[movex][movey] = 'V';
+                if(movey+1 == y)flag = true;
+            }else if((movey-1>=0) && labyrinth[movex][movey-1]=='0'){
+                movey-=1;
+                stack.push(String.valueOf(labyrinth[movex][movey]));
+                if(labyrinth[movex][movey]!='E')labyrinth[movex][movey] = 'V';
+                if(movey==0)flag = true;
+            }else{
+                System.out.println("Backtracking!");
+                if(stack.peek().equals("E")){
+                    System.out.println("The labyrinth has no exit");
+                    flag = true;
+                }
+                stack.pop();
+            }
+        }
+
+        System.out.println("--------------------------------");
+        for(i=0;i<x;i++){
+            
+            for(j=0;j<y;j++){
+                    System.out.print(labyrinth[i][j]);
+
+             }
+             System.out.println("\n"); 
+           }
+        
+
        }catch(IOException ex){
            System.out.println(ex.getMessage()+" IOException");
            System.exit(0);
        }
-
+       
        
     }
 }
